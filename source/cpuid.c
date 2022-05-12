0e01e8b0c8e033a35234afdf8435913891fcd8f4
Edison Hua <iseahound@gmail.com>
2022-05-12 18:20:35 -0400

    C source files

--- source/cpuid.c
+++ source/cpuid.c
@@ -1,19 +1,9 @@
-﻿void CPUID(unsigned long * a, unsigned long * b, unsigned long * c, unsigned long * d)
-{
-	__asm
-	{
-		mov	eax, DWORD PTR [a]
-		mov	eax, DWORD PTR [eax]
-		cpuid
-		push	eax
-		mov	eax, DWORD PTR [b]
-		mov	DWORD PTR [eax], ebx
-		mov	eax, DWORD PTR [c]
-		mov	DWORD PTR [eax], ecx
-		mov	eax, DWORD PTR [d]
-		mov	DWORD PTR [eax], edx
-		pop	eax
-		mov	edx, DWORD PTR [a]
-		mov	DWORD PTR [edx], eax
-	}
+﻿void native_cpuid(unsigned int *eax, unsigned int *ebx, unsigned int *ecx, unsigned int *edx) {
+        /* ecx is often an input as well as an output. */
+        asm volatile("cpuid"
+            : "=a" (*eax),
+              "=b" (*ebx),
+              "=c" (*ecx),
+              "=d" (*edx)
+            : "0" (*eax), "2" (*ecx));
 }
