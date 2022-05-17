56667fcedd9912b617b9ce1f5fbfe07e5ba19201
Edison Hua <iseahound@gmail.com>
2022-05-16 21:05:25 -0400

    C source files

--- source/setalpha.c
+++ source/setalpha.c
@@ -1,6 +1,6 @@
 void setalpha(unsigned int * start, unsigned int * end, unsigned char alpha) {
     while (start < end) {
-        *((char *) start + 3) = alpha;
+        *((unsigned char *) start + 3) = alpha;
         start++;
     }
 }
