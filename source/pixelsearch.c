a2ec455e03d0ffb5636df86ffdcf26103b1f18ce
Edison Hua <iseahound@gmail.com>
2023-09-05 19:25:21 -0400

    C source files

--- source/pixelsearch.c
+++
@@ -1,8 +0,0 @@
-unsigned int * pixelsearch(unsigned int * start, unsigned int * end, unsigned int color) {
-    while (start < end) {
-        if (*start == color)
-            return start;
-        start++;
-    }
-    return start; // start == end if no match.
-}
