6ed121f6b762e8582f24d5e06e6bc2f149c513fb
Edison Hua <iseahound@gmail.com>
2023-12-14 17:30:02 -0500

    Restore Default Behavior of ImageSearch

--- source/imagesearch1.c
+++ source/imagesearch1.c
@@ -1,51 +1,56 @@
-unsigned int * imagesearch1(unsigned int * start, unsigned int width, unsigned int height, unsigned int * s, unsigned int w, unsigned int h) {
+unsigned int * imagesearch1(unsigned int * start, unsigned int width, unsigned int height, unsigned int * s, unsigned int w, unsigned int h, unsigned int x, unsigned int y) {
     // width, height, start, current, end refer to the haystack (main image)
     // x, y, w, h, s, c, e refer to the needle (search image)
 
+    // Colors to be matched.
+    // c1 — c2
+    // |     |
+    // c3 — c4
+    unsigned int c1 = *(s);
+    unsigned int c2 = *(s + x);
+    unsigned int c3 = *(s + y * w);
+    unsigned int c4 = *(s + x + y * w);
+
+    // Search up to the first row of the remaining height. 
+    unsigned int * end = start + width * (height - h + 1); // y_domain
     unsigned int * current = start;
-    unsigned int * end = start + width * (height - h); // Remaining area must be greater than search height
-
-    int range_x = width - w;
-    int range_y = height - h; // optimized away
-
-    int x, y, offset;
-    unsigned int * c;
-    unsigned int * e;
-    unsigned int * p;
+    unsigned int x_domain = width - w;
 
     // Start off searching with pointers.
     while (current < end) {
 
-        // Check if the current pixel matches the first pixel of subimage.
-        if (*current == *s || *((unsigned char *) s + 3) == 0) { // just continue if search image is transparent
+        unsigned int offset = current - start; // Don't need to divide by 4 here.
+        unsigned int left = offset % width;
+        unsigned int top = offset / width; // optimized away
 
-            // Convert current pointer to (x, y).
-            offset = current - start;
-            x = offset % width;
-            y = offset / width; // optimized away
-
-            // Check if (x, y) exceeds bounds.
-            if (x > range_x) // range_y check is done above
-                goto next;
+        // Checks before subimage loop.
+        if (c4 == *(current + x + y * width) // Rank 1 - Focused Pixel
+        && (c1 == *(current))                // Rank 3 - First Pixel
+        && (left <= x_domain)                // Rank 2 - X-Range Check (Note: Assembly says this is 2nd)
+        // && c2 == *(current + x)
+        // && c3 == *(current + y * width)
+        ) {
 
             // Subimage loop.
-            c = s;
+            unsigned int * c = s;
             for (int i = 0; i < h; i++) {
-                p = current + width * i;
-                e = c + w;
+                unsigned int * p = current + width * i;
+                unsigned int * e = c + w;
                 while (c < e) {
-                    if (*((unsigned char *) c + 3)) { // skip transparent pixels in search image
+                    if (*((unsigned char *) c + 3)) { // Skip transparent pixels
                         if (*c != *p)
                             goto next;
                     }
-                    c++; // Here simply incrementing will interate the entire image
-                    p++; // Will be reset each run
+                    c++; // Iterate over the template image
+                    p++; // Reset pointer for each scanline
                 }
             }
             return current;
         }
+
         next:
         current++;
     }
+
     return start + width * height; // real end
 }
