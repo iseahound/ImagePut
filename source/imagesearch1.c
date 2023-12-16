4161cc1473f957eda4bdb891049e5c310997e899
Edison Hua <iseahound@gmail.com>
2023-12-15 22:32:11 -0500

    Add files via upload
    Finalize ImageSearch1 code to be efficient and bug-free.
    Supports transparency properly.
    Fast!

--- source/imagesearch1.c
+++ source/imagesearch1.c
@@ -1,46 +1,47 @@
 unsigned int * imagesearch1(unsigned int * start, unsigned int width, unsigned int height, unsigned int * s, unsigned int w, unsigned int h, unsigned int x, unsigned int y) {
-    // width, height, start, current, end refer to the haystack (main image)
-    // x, y, w, h, s, c, e refer to the needle (search image)
+    // source: left, top, width, height, start, current, end refer to the haystack (main image) 
+    // target: x, y, w, h, s, c, e refer to the needle (search or template image)
 
-    // Colors to be matched.
-    // c1 — c2
-    // |     |
-    // c3 — c4
-    unsigned int c1 = *(s);
-    unsigned int c2 = *(s + x);
-    unsigned int c3 = *(s + y * w);
-    unsigned int c4 = *(s + x + y * w);
+    int trans = (*((unsigned char *) s + 3) == 0);  // Check if top-left pixel is transparent
 
-    // Search up to the first row of the remaining height. 
-    unsigned int * end = start + width * (height - h + 1); // y_domain
+    unsigned int c1 = *(s);                         // ↓ Top-left pixel
+    unsigned int c2 = *(s + x);                     // c1 — c2
+    unsigned int c3 = *(s + y * w);                 // |     |
+    unsigned int c4 = *(s + x + y * w);             // c3 — c4 ← Focused pixel
+    
+    unsigned int x_domain = width - w;              // Avoid search of the narrow edge on the right-hand side
+    unsigned int y_domain = height - h + 1;         // Add 1 to search at least one row
+
+    unsigned int * end = start + width * y_domain;  // Remaining area must be greater than search height
     unsigned int * current = start;
-    unsigned int x_domain = width - w;
 
     // Start off searching with pointers.
     while (current < end) {
 
-        unsigned int offset = current - start; // Don't need to divide by 4 here.
-        unsigned int left = offset % width;
-        unsigned int top = offset / width; // optimized away
+        unsigned int offset = current - start;      // Don't need to divide by 4 here.
+        unsigned int left = offset % width;         // Get x coordinate of source image
+        unsigned int top = offset / width;          // optimized away
 
-        // Checks before subimage loop.
-        if (c4 == *(current + x + y * width) // Rank 1 - Focused Pixel
-        && (c1 == *(current))                // Rank 3 - First Pixel
-        && (left <= x_domain)                // Rank 2 - X-Range Check (Note: Assembly says this is 2nd)
-        // && c2 == *(current + x)
-        // && c3 == *(current + y * width)
-        ) {
-
-            // Subimage loop.
+        // Rank Reasoning
+        // 1. The focused pixel has the most entropy and is least likely to match the source.
+        //    CANNOT BE TRANSPARENT.
+        // 2. The top-left pixel may match the source.
+        // 3. The x-domain check determines if the match is too far to the right.
+        //    Advancing the pointer by w - 1 makes little difference.
+        if (c4 == *(current + x + y * width))       // Rank 1 - Focused Pixel
+        if (trans || c1 == *(current))              // Rank 2 - Top-left Pixel
+        if (left <= x_domain)                       // Rank 3 - X-Domain Check
+        {
+            // Subimage matching loop.
             unsigned int * c = s;
             for (int i = 0; i < h; i++) {
                 unsigned int * p = current + width * i;
                 unsigned int * e = c + w;
-                while (c < e) {
-                    if (*((unsigned char *) c + 3)) { // Skip transparent pixels
+                while (c < e) {                     // Scan line-by-line
+                    if (*((unsigned char *) c + 3)) // Skip transparent pixels
                         if (*c != *p)
                             goto next;
-                    }
+
                     c++; // Iterate over the template image
                     p++; // Reset pointer for each scanline
                 }
