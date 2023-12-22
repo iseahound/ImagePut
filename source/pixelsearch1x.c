c021c8d454fff74e2e7b3d3b627835589622f59a
Gunwoo Gim (il debole) <wind8702@gmail.com>
2023-12-22 19:40:10 +0900

    Experiment with instruction level parallelism

--- source/pixelsearch1x.c
+++ source/pixelsearch1x.c
@@ -1,29 +1,40 @@
 #include <emmintrin.h>
 
+// The number of vector registors to utilize in each innermost loop
+#define NR_VEC 2
+
 unsigned int * pixelsearch1x(unsigned int * start, unsigned int * end, unsigned int color) {
+    // The number of elements in each vector register
+    size_t nr_elems_in_vec = sizeof(__m128i) / sizeof(*start);
 
     // Create a vector of four copies of the target color.
     __m128i vcolor = _mm_set1_epi32(color);
 
     // Loop over start pointer with a step of four unsigned integers.
-    while (start < end - 3) {
+    while ( start <  end - (nr_elems_in_vec * NR_VEC) + 1 ) {
+        int mask[NR_VEC];
 
-        // Load four unsigned integers from start into a vector.
-        __m128i vstart = _mm_loadu_si128((__m128i *) start);
+        for ( int i = 0; i < NR_VEC; i++ ) {
+            // Load four unsigned integers from start into a vector.
+            __m128i vstart = _mm_loadu_si128( ((__m128i *) start) + i );
 
-        // Compare vstart and vcolor for equality.
-        __m128i vcmp = _mm_cmpeq_epi32(vstart, vcolor);
+            // Compare vstart and vcolor for equality.
+            __m128i vcmp = _mm_cmpeq_epi32(vstart, vcolor);
 
-        // Create a mask from each byte (using the most significant bit) in vcmp.
-        int mask = _mm_movemask_epi8(vcmp);
+            // Create a mask from each byte (using the most significant bit) in vcmp.
+            mask[i] = _mm_movemask_epi8(vcmp);
+        }
 
-        // If the mask is nonzero, there is at least one match.
-        if (mask != 0)
-            break;
+        for ( int i = 0; i < NR_VEC; i++ ) {
+            // If the mask is nonzero, there is at least one match.
+            if (mask[i] != 0)
+                goto end_vector_loop;
+        }
 
         // Increment start by four unsigned integers.
-        start += 4;
+        start += sizeof(__m128i) / sizeof(*start) * NR_VEC;
     }
+end_vector_loop:
 
     // Clean up any remaining elements.
     while (start < end) {
